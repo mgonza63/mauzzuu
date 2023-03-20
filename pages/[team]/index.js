d787aab7e5be2d92f53e4900d6477488466471cc
Mauricio Gonzalez <57790156+mgonza63@users.noreply.github.com>
2023-03-20 17:15:02 -0500

    testing meta image

--- pages/[team]/index.js
+++ pages/[team]/index.js
@@ -6,15 +6,15 @@ import {useEffect} from 'react'
 export default function TeamPage() {
     const router = useRouter()
     const { p1, p2 } = router.query
-    useEffect(() => {
-        window.location.assign('https://jebbit.com/')
-    })
+    // useEffect(() => {
+    //     window.location.assign('https://jebbit.com/')
+    // })
     return (
         <div>
             <Head>
                 <title>{p1}</title>
                 <meta property="og:title" content={p2} key="title" />
-                <meta property="og:image" content={`https://mauzzuu.vercel.app//api/nba01?p1=${p1}&p2=${p2}`} />
+                <meta property="og:image" content={`https://mauzzuu.vercel.app/api/nba01?p1=${p1}&p2=${p2}`} />
 
             </Head>
             {p1}
