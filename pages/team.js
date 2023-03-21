80fae85e5193c7cc3aeba3706656627d414077ea
Mauricio Gonzalez <57790156+mgonza63@users.noreply.github.com>
2023-03-21 11:20:04 -0500

    added redirect to team.js

--- pages/team.js
+++ pages/team.js
@@ -1,5 +1,9 @@
 import { useRouter } from 'next/router'
+import { Inter } from 'next/font/google'
+import { useEffect } from 'react'
 import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export async function getServerSideProps({params, query}) {
         console.log(query)
@@ -14,9 +18,9 @@ export default function TeamPage({ players }) {
     // const router = useRouter()
     // let { p1, p2 } = router.query
 
-    // useEffect(() => {
-    //     window.location.assign('https://jebbit.com/')
-    // })
+    useEffect(() => {
+        window.location.assign('https://jebbit.com/')
+    })
 
     let formatPlayerName = (name) => {
         const formattedName = name.replace(/_/g, ' ').replace(/(?: |\b)(\w)/g, function(key, p1) {
@@ -27,7 +31,7 @@ export default function TeamPage({ players }) {
     let metaTitle = `I've selected my 2023 All-Star Roster! ${formatPlayerName(players.p1)} is my captain`
     let metaDescription = 'Draft yours and receive a custom graphic like this one with your selections!'
     return (
-        <div>
+        <div className='wrapper'>
             <Head>
                 <title>{metaTitle}</title>
                 <meta property="description" content={metaDescription} />
@@ -43,7 +47,20 @@ export default function TeamPage({ players }) {
                 <meta name="twitter:image" content={`https://mauzzuu.vercel.app/api/nba01?p1=${players.p1}&p2=${players.p2}`}/>
 
             </Head>
-            <div>Loading...</div>
+            <div className={inter.className}>Loading...</div>
+            <img src='https://uploads-ssl.webflow.com/61533c36571fb3795e94c3ef/61534400571fb372f594ed25_jebbit-logo-color.png' />
+            <style jsx>{`
+                .wrapper {
+                    height: 100vh;
+                    display: grid;
+                    place-items: center;
+                    grid-template-rows: 1fr auto;
+                }
+                img {
+                    width: 150px;
+                    padding: 2rem;
+                }
+            `}</style>
         </div>
         
     )
