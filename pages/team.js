ec7024bb2ae125424b6e81c187801ecc2eddcd26
Mauricio Gonzalez <57790156+mgonza63@users.noreply.github.com>
2023-03-20 23:33:46 -0500

    added more twitter meta tags

--- pages/team.js
+++ pages/team.js
@@ -30,14 +30,16 @@ export default function TeamPage({ players }) {
         <div>
             <Head>
                 <title>{metaTitle}</title>
-                <meta property="og:title" content={metaTitle} key="title" />
-                <meta name="twitter:title" content={metaTitle} />
-
                 <meta property="description" content={metaDescription} />
-                <meta property="og:description" content={metaDescription} />
-                <meta name="twitter:description" content={metaDescription}/>
 
+                <meta property="og:title" content={metaTitle} key="title" />
+                <meta property="og:description" content={metaDescription} />
                 <meta property="og:image" content={`https://mauzzuu.vercel.app/api/nba01?p1=${players.p1}&p2=${players.p2}`} />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@mau_jebbit" />
+                <meta name="twitter:title" content={metaTitle} />
+                <meta name="twitter:description" content={metaDescription}/>
                 <meta name="twitter:image" content={`https://mauzzuu.vercel.app/api/nba01?p1=${players.p1}&p2=${players.p2}`}/>
 
             </Head>
